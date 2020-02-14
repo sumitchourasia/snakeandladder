22ec6c99081ad2d175d1d7477072bc2afd42afc7
sumit <st.sumit01@gmail.com>
2020-02-14 18:24:40 +0530

    [sumit]Refactor . removed the unneccessary file

--- snakeandladder/snakeandladder.sh
+++ snakeandladder/snakeandladder.sh
@@ -1,46 +1,15 @@
 
-function InitializeBoard()
-{
-	value=100
-        for ((i=0; i<10 ;i++ ))
-        do
-                if [ "$(($i % 2))" -eq "0" ]
-                then
-                        for(( j=0;j<10;j++ ))
-                        do
-                                board[$i,$j]=$value
-                                #echo -e "${board[$i,$j]} \c"
-                                value=$(( $value - 1 ))
-                        done
-                else
-                        for (( j=9;j>=0;j-- ))
-                        do
-                                board[$i,$j]=$value
-                                value=`expr $value - 1`
-                        done
-                fi
-        done
+function GenerateRandom()
+{	
+	random=$(($RANDOM % 6))
+	random=$(($random + 1))
+	return $random
 }
 
-
-function PrintBoard()
+function RollTheDie()
 {
-        for (( i=0;i<10;i++ ))
-        do
-                if [ "$i" -eq "9" ]
-                then
-                         for ((j=0;j<10;j++))
-                         do
-                                echo -e " ${board[$i,$j]}  \c "
-                         done
-                else
-                        for ((j=0;j<10;j++))
-                        do
-                        echo -e " ${board[$i,$j]} \c "
-                        done
-                fi
-                echo " "
-                echo " "
-        done
+	GenerateRandom
+	randomoutput=$?
+	return $randomoutput
 }
 
