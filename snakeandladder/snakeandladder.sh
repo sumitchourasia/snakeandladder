384e065017b9fc4eec59868f4af1bd129bb6489f
sumit <st.sumit01@gmail.com>
2020-02-14 18:35:42 +0530

    [sumit]Refactor . modified upon last commit

--- snakeandladder/snakeandladder.sh
+++ snakeandladder/snakeandladder.sh
@@ -1,15 +1,28 @@
-
-function GenerateRandom()
-{	
-	random=$(($RANDOM % 6))
-	random=$(($random + 1))
-	return $random
-}
-
-function RollTheDie()
+function InitializeBoard()
 {
-	GenerateRandom
-	randomoutput=$?
-	return $randomoutput
+
+
 }
 
+
+
+function PrintBoard()
+{
+        for (( i=0;i<10;i++ ))
+        do
+                if [ "$i" -eq "9" ]
+                then
+                         for ((j=0;j<10;j++))
+                         do
+                                echo -e " ${board[$i,$j]}  \c "
+                         done
+                else
+                        for ((j=0;j<10;j++))
+                        do
+                        echo -e " ${board[$i,$j]} \c "
+                        done
+                fi
+                echo " "
+                echo " "
+        done
+}
