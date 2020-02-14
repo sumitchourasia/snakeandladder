441fb4438fe101523c8a438d8c7fd2217788cb26
sumit <st.sumit01@gmail.com>
2020-02-14 17:43:29 +0530

    [sumit]Refactor . adding function InitializeBoard

--- snakeandladder/snakeandladder.sh
+++ snakeandladder/snakeandladder.sh
@@ -1,2 +1,24 @@
-echo "checking"
-echo "skadg"
+value=100
+function InitializeBoard()
+{
+        local i=0;
+        for ((i=0; i<10 ;i++ ))
+        do
+                if [ "$(($i % 2))" -eq "0" ]
+                then
+                        for(( j=0;j<10;j++ ))
+                        do
+                                board[$i,$j]=$value
+                                #echo -e "${board[$i,$j]} \c"
+                                value=$(( $value - 1 ))
+                        done
+                else
+                        for (( j=9;j>=0;j-- ))
+                        do
+                                board[$i,$j]=$value
+                                value=`expr $value - 1`
+                        done
+                fi
+        done
+}
+
