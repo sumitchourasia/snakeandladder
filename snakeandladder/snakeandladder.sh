fd6b8456d0de0b6fba723f3ffe8329ea1385f707
sumit <st.sumit01@gmail.com>
2020-02-14 18:08:34 +0530

    [sumit]Add . add the function GenerateRandom and RollTheDie

--- snakeandladder/snakeandladder.sh
+++ snakeandladder/snakeandladder.sh
@@ -1,4 +1,15 @@
+
+function GenerateRandom()
+{	
+	random=$(($RANDOM % 6))
+	random=$(($random + 1))
+	return $random
+}
+
 function RollTheDie()
 {
-
+	GenerateRandom
+	randomoutput=$?
+	return $randomoutput
 }
+
