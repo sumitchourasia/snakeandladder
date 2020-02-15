e0a10fecd189f71da59ef5b765c91cbc673fb514
sumit <st.sumit01@gmail.com>
2020-02-15 19:45:24 +0530

    [sumit]Add . adding useCase-6

--- snakeandladder/snakeandladder.sh
+++ snakeandladder/snakeandladder.sh
@@ -1,5 +1,38 @@
-#!/bin/bash
-declare -A board[10,10]
-#set -x
-# sanke and ladder program implementation
-echo "welcome to snake and ladder game"
+function CaseStatement()
+{
+	times=0
+        while [ $currentposition -lt 100 ]
+        do
+        GenerateRandom
+        random=$?
+        option=$(($random % 11))
+
+        if [ "$option" -eq "0" ]
+        then
+                option=0
+        elif [ "$(($option % 5))" -eq "0" ]
+        then
+                option=2
+        else
+                option=1
+        fi
+	times=$(( $times + 1 ))
+        case $option in
+                0)
+                        echo "do nothing"
+                        ;;
+                1)
+                        echo "ladder"
+                        ladder
+                        PrintBoard
+                        ;;
+                2)
+                        echo "snake"
+                        Snake
+                        PrintBoard
+                        ;;
+
+        esac
+        done
+	echo "total number of times : $times"
+}
