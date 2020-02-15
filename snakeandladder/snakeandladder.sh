addf4cac8a260babd803cf80d21a2cce07563624
sumit <st.sumit01@gmail.com>
2020-02-15 17:32:54 +0530

    [sumit]Refactor . modified for optimization

--- snakeandladder/snakeandladder.sh
+++ snakeandladder/snakeandladder.sh
@@ -6,33 +6,39 @@ echo "welcome to snake and ladder game"
 
 function InitializeBoard()
 {
-	value=100
-	for (( i=0;i<10;i++ ))
-	do
-		for(( j=0;j<10;j++ ))
-		do
-		board[$i,$j]=$value
-		value=$(($value - 1))
-		done
-	done
-}
-
-function PrintBoard()
-{
-        for (( i=0;i<10;i++ ))
+        value=100
+        for (( i=9;i>=0;i-- ))
         do
-                if [ "$i" -eq "9" ]
+                if [ "$(($i % 2 ))" -eq "0" ]
                 then
-                         for ((j=0;j<10;j++))
+                         for ((j=9;j>=0;j--))
                          do
-                                echo -e " ${board[$i,$j]}  \c "
+                                board[$i,$j]="$value"
+                                value=$(( $value - 1 ))
                          done
                 else
                         for ((j=0;j<10;j++))
                         do
-                        echo -e " ${board[$i,$j]} \c "
+                                board[$i,$j]="$value"
+                                value=$(( $value - 1 ))
                         done
                 fi
+                done
+}
+
+function PrintBoard()
+{
+        for (( i=9;i>=0;i-- ))
+        do
+                for(( j=0;j<10;j++ ))
+                do
+                        if [ "$i" -eq "0" ]
+                        then
+                                echo -e "  ${board[$i,$j]}   \c"
+                        else
+                                echo -e "  ${board[$i,$j]}  \c"
+                        fi
+                done
                 echo " "
                 echo " "
         done
@@ -84,56 +90,64 @@ function CaseStatement()
         done
 }
 
-function ladder()
-{ 
-	RollTheDie
-	die=$?
-	die=$(( $die % 6 ))
-	die=$(($die + 1))
-	temp=$(( $currentposition + $die ))
-	previousposition=$currentposition
-	i=$(($temp / 10 ))
-	j=$(( $temp % 10 ))
-	
-	if [ "$j" -eq "0" ]
-	then
-		i=$(( $i - 1 ))
-		j=9
-	else
-		if [ "$(( $i % 2 ))" -eq "0" ]
-		then
-		j=$(( $j - 1 ))
-		else
-			j=$(( 9 - $j + 1 ))
-		fi
-	fi
-	echo "currentpostion : $currentposition"
-	currentposition=$temp
-	echo "new position  : $currentposition"
-	board[$i,$j]="p1"
-	
-	PutPrevious
 
+function ladder()
+{
+        RollTheDie
+        die=$?
+        die=$(($die % 6))
+        die=$(($die + 1))
+        previousposition=$currentposition
+        currentposition=$(( $currentposition + $die ))
+        i=$(($currentposition / 10 ))
+        j=$(($currentposition % 10 ))
+
+        if [ "$j" -eq "0" ]
+        then
+                if [ "$(($i % 2))" -eq "0" ]
+                then
+                         i=$(( $i - 1))
+                else
+                         i=$(( $i - 1 ))
+                         j=9
+                fi
+        else
+                if [ "$(( $i % 2 ))" -eq "0" ]
+                then
+                j=$(( $j - 1 ))
+                else
+                        j=$(( 9 - $j + 1 ))
+                fi
+        fi
+        echo "currentpostion : $previousposition"
+        echo "new position  : $currentposition"
+        board[$i,$j]="p1"
+        PutPrevious
 }
 
 function PutPrevious()
 {
- 	if [ "$previousposition" -eq "0" ]
-	then
-		previousposition=1
-	fi
-	pi=$(($previousposition / 10 ))
+        if [ "$previousposition" -eq "0" ]
+        then
+                previousposition=1
+        fi
+        pi=$(($previousposition / 10 ))
         pj=$(($previousposition % 10 ))
-	echo "previous position : $previousposition"
+        echo "previous position : $previousposition"
 
-	if [ "$pj" -eq "0" ]
-        then	
-		if [ "$pi" -eq "0" ]
-		then
-			pi=1
-		fi
-                pi=$(( $pi - 1 ))
-                pj=9
+        if [ "$pj" -eq "0" ]
+        then
+                if [ "$pi" -eq "0" ]
+                then
+                        pi=1
+                fi
+                if [ "$(($pi % 2))" -eq "0" ]
+                then
+                         pi=$(( $i - 1))
+                else
+                         pi=$(( $i - 1 ))
+                         pj=9
+                fi
         else
                 if [ "$(($pi % 2))" -eq "0" ]
                 then
@@ -142,7 +156,7 @@ function PutPrevious()
                         pj=$(( 9 - $pj + 1 ))
                 fi
         fi
-	echo " pi and pj :  $pi , $pj"
-	board[$pi,$pj]=$previousposition
+        echo " pi and pj :  $pi , $pj"
+        board[$pi,$pj]=$previousposition
 }
 
